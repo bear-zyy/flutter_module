0e445e0bbcbf7f2c0acbe377d988563546b76476
bear-zyy <13520592875@163.com>
2020-01-03 18:42:40 +0800

    新增页面

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,38 @@
 import 'package:flutter/material.dart';
+import 'dart:ui';
+import 'package:flutter/services.dart';
+import 'secondWidget.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MyApp(window.defaultRouteName));
 
-Widget flutterView(){
-  return Container(
-    width: 375,
-    height: 667,
-    color: Colors.red,
-    child: Center(
-      child: Container(
-        width: 200,
-        height: 200,
-        color: Colors.white,
-        child: Center(
-          child: Container(
-            height: 100,
-            width: 100,
-            color: Colors.yellow,
-          ),
-        ),
-      ),
-    ),
-
-  );
-}
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+Widget MyApp(String route){
+  switch (route){
+    case 'firstWidget':
+      return MyHomePage(title: "firstWidget",);
+    case 'secondWidget' :
+      return secondWidget();
+    default:
+      return Container(
+        width: 375,
+        height: 667,
+        color: Colors.orange,
+      );
   }
 }
+
+//class MyApp extends StatelessWidget {
+//  // This widget is the root of your application.
+//  @override
+//  Widget build(BuildContext context) {
+//    return MaterialApp(
+//      title: 'Flutter Demo',
+//      theme: ThemeData(
+//        primarySwatch: Colors.blue,
+//      ),
+//      home: MyHomePage(title: 'Flutter Demo Home Page'),
+//    );
+//  }
+//}
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
@@ -49,7 +44,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
 
   @override
   Widget build(BuildContext context) {
---
+++ lib/secondWidget.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+Widget secondWidget(){
+  return Container(
+    color: Colors.grey,
+    width: 375,
+    height: 667,
+  );
+}
